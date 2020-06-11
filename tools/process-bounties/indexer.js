33a9a1f5f6d04154a5276f62f054b8348b9f6b91
Adam Nygate <adam@418sec.com>
2020-06-11 11:55:38 +0100

    Fixed indexer

--- tools/process-bounties/indexer.js
+++ tools/process-bounties/indexer.js
@@ -1,47 +1,46 @@
 'use strict';
 
-const fs = require("fs");
-const { promisify } = require("util");
+const fs = require("fs/promises");
 
-const fdir = require("fdir").default;
+const fdir = require("fdir");
 
 const homeDir = "../../"
+const bountyDir = homeDir + "bounties"
 
 const rootIndexFilename = "index.json"
 const rootIndexPath = homeDir + rootIndexFilename
 
-const bountyDir = homeDir + "bounties"
 const vulnerabilityFilename = "vulnerability.json"
 
-const writeFileAsync = promisify(fs.writeFile)
+const bounties = new fdir()
+    .withBasePath()
+    .filter(path => path.includes("bounty.json"))
+    .crawl(bountyDir)
 
-// Find all open bounties
-fdir.async(bountyDir, {
-        searchFn: path => path.includes("bounty.json")
-    })
-    .then(async bountyPaths => {
-        var bounties = [];
+bounties.withPromise().then(async bountyPaths => {
+    var bountiesToIndex = []
 
-        // Iterate through the bounties and add them to the index array
-        bountyPaths.forEach(bountyPath => {
-            const bountyDir = bountyPath.split("bounty.json")[0];
+    // Iterate through the bounties and add them to the index array
+    for (const bountyPath of bountyPaths) {
+        const bountyDir = bountyPath.split("/bounty.json")[0]
+
+        const bountyDetails = await fs.readFile(bountyPath, 'utf8').then(JSON.parse)        
+        const vulnerabilityDetails = await fs.readFile(`${bountyDir}/${vulnerabilityFilename}`, 'utf8').then(JSON.parse)
         
-            const vulnerability = require(bountyDir + vulnerabilityFilename);
-            const bounty = require(bountyPath);
-            
-            bounties.push({
-                "Registry": vulnerability.Package.Registry,
-                "PackageName": vulnerability.Package.Name,
-                "PackageVulnerabilityID": vulnerability.PackageVulnerabilityID,
-                "CodebasePrimaryLanguage": vulnerability.Repository.Codebase[0],
-                "VulnerabilityDescription": vulnerability.Summary,
-                "Severity": vulnerability.CVSS.Score,
-                "AffectedVersionRange": vulnerability.AffectedVersionRange,
-                "DisclosureDate": vulnerability.DisclosureDate,
-                "Bounty": bounty.Bounty
-            });
-        })
         
-        // Write bounties to the index file
-        await writeFileAsync(rootIndexPath, JSON.stringify(bounties));
-    })
+        bountiesToIndex.push({
+            "Registry": vulnerabilityDetails.Package.Registry,
+            "PackageName": vulnerabilityDetails.Package.Name,
+            "PackageVulnerabilityID": vulnerabilityDetails.PackageVulnerabilityID,
+            "CodebasePrimaryLanguage": vulnerabilityDetails.Repository.Codebase[0],
+            "VulnerabilityDescription": vulnerabilityDetails.Summary,
+            "Severity": vulnerabilityDetails.CVSS.Score,
+            "AffectedVersionRange": vulnerabilityDetails.AffectedVersionRange,
+            "DisclosureDate": vulnerabilityDetails.DisclosureDate,
+            "Bounty": bountyDetails.Bounty
+        });
+    }
+    
+    // Write bounties to the index file
+    await fs.writeFile(rootIndexPath, JSON.stringify(bounties));
+})
