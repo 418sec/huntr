684cabb487b3060c8760017e8ae5afd9fe09f382
Adam Nygate <adam@418sec.com>
2020-06-12 10:32:53 +0100

    re-enrich imagickal and fix auth for octokit

--- tools/process-bounties/enricher.js
+++ tools/process-bounties/enricher.js
@@ -10,7 +10,7 @@ const homeDir = "../../"
 const bountyDir = homeDir + "bounties"
 
 const octokit = new Octokit({
-    auth: process.env.HUNTR_HELPER_TOKEN
+    auth: 'token ' + process.env.HUNTR_HELPER_TOKEN
 })
 
 const bounties = new fdir()
