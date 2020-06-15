8da7e50aebfd2b24a9f1e24de53f29ed34665e88
Adam Nygate <4394852+adam-nygate@users.noreply.github.com>
2020-06-15 15:27:34 +0100

    Publishing bounties (#59)
    * WIP process-bounty action
    
    * Fixed checkout action
    
    * Trigger workflow
    
    * Create README.md
    
    * Create vulnerability.json
    
    * Create bounty.json
    
    * Remove Test Vulnerability
    
    This commit removes the test vulnerability from the bounties listing.
    
    * Create README.md
    
    * Create vulnerability.json
    
    * Update vulnerability.json
    
    * Test fix of workflow
    
    * Testfix
    
    * Trigger workflow
    
    * Revert "TEST"
    
    * Fixed a typo and preventing the workflow from running when unnecessary
    
    * Upload commit-msg
    
    * Create README.md
    
    * Create vulnerability.json
    
    * Update vulnerability.json
    
    * Refactored workflow
    
    * Correct bounty
    
    * Correct typo
    
    * Prepping for re-enrichment
    
    * Fixed indexer
    
    * Prepping for enrichment
    
    * Fix push to staging
    
    * Prepping for re-enrichment
    
    * Regenerated the bounty index
    
    * Ready for testing
    
    * Ready for testing
    
    * Actually ready for testing
    
    * Re-enriching curlrequest
    
    * Replaced token with huntr-helper token
    
    * re-enrich jison
    
    * re-enrich imagickal and fix auth for octokit
    
    * Test enrichment
    
    * Test re-enrich
    
    * Regenerated the bounty index
    
    * Fix template when no vuln author
    
    * Re-enrich safereval
    
    * Regenerated the bounty index
    
    * Update templates
    
    * Re-enrich cinder
    
    * Regenerated the bounty index
    
    * Made json files more readable
    
    * Modified commit message
    
    * Trigger re-enrichment
    
    * Enriched bounties and regenerated the bounty index
    
    * Removed limit of enrichment to 1 bounty
    
    * Trigger global re-enrichment
    
    * Enriched bounties and regenerated the bounty index
    
    * Cleaned repository
    
    * Fixed indexer
    
    * Fixed credits reward for cinder
    
    * Enriched bounties and regenerated the bounty index
    
    Co-authored-by: Jamie Slome <55323451+JamieSlome@users.noreply.github.com>
    Co-authored-by: huntr-helper <61279246+huntr-helper@users.noreply.github.com>

--- tools/enricher/README.md
+++
@@ -1,7 +0,0 @@
-# Overview
-This enriches bounties that have been pushed to the staging branch.
-
-Enrichment includes:
-- Opening GitHub Issues (if none exist)
-- Creating GitHub Comments (if no Issue exists)
-- Creating a 418sec fork of the original repository
---
+++ tools/process-bounties/indexer.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require("fs/promises");
+
+const fdir = require("fdir");
+
+const homeDir = "../../"
+const bountyDir = homeDir + "bounties"
+
+const rootIndexFilename = "index.json"
+const rootIndexPath = homeDir + rootIndexFilename
+
+const vulnerabilityFilename = "vulnerability.json"
+
+const bounties = new fdir()
+    .withBasePath()
+    .filter(path => path.includes("bounty.json"))
+    .crawl(bountyDir)
+
+bounties.withPromise().then(async bountyPaths => {
+    var bountiesToIndex = []
+
+    // Iterate through the bounties and add them to the index array
+    for (const bountyPath of bountyPaths) {
+        const bountyDir = bountyPath.split("/bounty.json")[0]
+
+        const bountyDetails = await fs.readFile(bountyPath, 'utf8').then(JSON.parse)        
+        const vulnerabilityDetails = await fs.readFile(`${bountyDir}/${vulnerabilityFilename}`, 'utf8').then(JSON.parse)
+        
+        
+        bountiesToIndex.push({
+            "Registry": vulnerabilityDetails.Package.Registry,
+            "PackageName": vulnerabilityDetails.Package.Name,
+            "PackageVulnerabilityID": vulnerabilityDetails.PackageVulnerabilityID,
+            "CodebasePrimaryLanguage": vulnerabilityDetails.Repository.Codebase[0],
+            "VulnerabilityDescription": vulnerabilityDetails.Summary,
+            "Severity": vulnerabilityDetails.CVSS.Score,
+            "AffectedVersionRange": vulnerabilityDetails.AffectedVersionRange,
+            "DisclosureDate": vulnerabilityDetails.DisclosureDate,
+            "Bounty": bountyDetails.Bounty
+        });
+    }
+    
+    // Write bounties to the index file
+    await fs.writeFile(rootIndexPath, JSON.stringify(bountiesToIndex));
+})
