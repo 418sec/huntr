0e16f24069594bfb993cd7e28fbf2174a986309a
Adam Nygate <adam@418sec.com>
2020-11-11 17:07:04 +0000

    Implemented @actions/core

--- tools/validator/index.js
+++ tools/validator/index.js
@@ -3,6 +3,8 @@ import {promisify} from "util";
 import {execFile} from "child_process";
 import {dirname} from "path";
 
+import * as core from "@actions/core";
+
 const promisifiedExecFile = promisify(execFile);
 
 // Get the git diff
@@ -16,7 +18,7 @@ const diffString = await promisifiedExecFile("git", [
 
 // Check for errors
 if (diffString.stderr)
-  console.error("Error whilst executing git diff", diffString.stderr);
+  core.error("Error whilst executing git diff", diffString.stderr);
 
 // Process git diff's stdout
 const diffStringLines = diffString.stdout
@@ -24,7 +26,7 @@ const diffStringLines = diffString.stdout
   .filter((e) => e); // This removes empty strings
 
 if (diffStringLines.length === 0)
-  console.error("Diff is empty.", diffString.stdout);
+  core.error("Diff is empty.", diffString.stdout);
 
 let diff = [];
 diffStringLines.forEach((line) => {
@@ -35,19 +37,29 @@ diffStringLines.forEach((line) => {
   });
 });
 
+let isValid = true;
+
 // Check for non-bounty files
-if (diff.filter((item) => item.path.startsWith("bounties/")))
-  console.error("Diff must only contain changes to bounty files.", diff);
+if (diff.filter((item) => item.path.startsWith("bounties/"))) {
+  core.error("Diff must only contain changes to bounty files.", diff);
+  isValid = false;
+}
 
 // Check for many new bounty directories
 const diffDirs = [...new Set(diff.map((item) => dirname(item.path)))];
-if (diffDirs.length > 1)
-  console.error("Diff must only contain one bounty directory.", diff);
+if (diffDirs.length > 1) {
+  core.error("Diff must only contain one bounty directory.", diff);
+  isValid = false;
+}
 
 // Check for only new bounties (additions)
-if (diff.filter((item) => item.change === "M" || "D"))
-  console.error("Diff must only contain new bounties.", diff);
+if (diff.filter((item) => item.change === "M" || "D")) {
+  core.error("Diff must only contain new bounties.", diff);
+  isValid = false;
+}
 
-console.log("Diff is valid.", diff);
-
-return {test: "hello"};
+if (isValid) {
+  core.info("Diff is valid.", diff);
+} else {
+  core.setFailed("Diff is invalid");
+}
