78ef49931a3beb84d212547f190b62d6ded67a6c
Adam Nygate <adam@418sec.com>
2020-11-12 10:48:51 +0000

    WIP json validator

--- tools/validator/contains-only-one-bounty.js
+++ tools/validator/contains-only-one-bounty.js
@@ -8,5 +8,7 @@ const diff = JSON.parse(process.env.DIFF);
 
 // Check for many new bounty directories
 const diffDirs = [...new Set(diff.map((item) => dirname(item.path)))];
-if (diffDirs.length > 1)
-  core.setFailed("Diff must only contain one bounty directory.");
+// if (diffDirs.length > 1)
+//   core.setFailed("Diff must only contain one bounty directory.");
+// else
+core.setOutput("bounty_dir", diffDirs[0]);
