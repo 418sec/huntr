0f28225e02be16220e0d33eb544f4756a2f198f2
Adam Nygate <adam@418sec.com>
2020-11-12 09:20:34 +0000

    test

--- tools/validator/contains-only-bounty-files.js
+++ tools/validator/contains-only-bounty-files.js
@@ -2,6 +2,8 @@
 
 import * as core from "@actions/core";
 
+const diff = JSON.parse(process.env.DIFF);
+
 // Check for non-bounty files
-if (process.env.DIFF.filter((item) => item.path.startsWith("bounties/")))
+if (diff.filter((item) => item.path.startsWith("bounties/")))
   core.setFailed("Diff must only contain changes to bounty files.");
