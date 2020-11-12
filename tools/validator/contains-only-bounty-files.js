9a778b9889dff14078e7ec2ae36e08b90526caa6
Adam Nygate <adam@418sec.com>
2020-11-12 09:17:41 +0000

    test

--- tools/validator/contains-only-bounty-files.js
+++ tools/validator/contains-only-bounty-files.js
@@ -1,11 +1,7 @@
 "use strict";
 
-// import * as core from "@actions/core";
+import * as core from "@actions/core";
 
-console.log(process.env.DIFF);
-
-// // Check for non-bounty files
-// if (process.env.DIFF.filter((item) => item.path.startsWith("bounties/"))) {
-//   core.warning("Diff must only contain changes to bounty files.");
-//   core.setFailed("Diff is invalid.");
-// }
+// Check for non-bounty files
+if (process.env.DIFF.filter((item) => item.path.startsWith("bounties/")))
+  core.setFailed("Diff must only contain changes to bounty files.");
