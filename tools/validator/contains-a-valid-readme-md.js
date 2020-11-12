5f401d905a8a94d45c0a80585fb97717a9511b1e
Adam Nygate <adam@418sec.com>
2020-11-12 13:15:29 +0000

    Added check for valid README.md

---
+++ tools/validator/contains-a-valid-readme-md.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import * as fs from "fs/promises";
+
+const vulnerabilityReadmePath = `../../${process.env.BOUNTY_DIR}/README.md`;
+
+// Try to read the README.md
+const vulnerabilityReadme = await fs
+  .readFile(vulnerabilityReadmePath, "utf8")
+  .catch((err) => {
+    core.error(err);
+    core.setFailed(`A README.md does not exist in ${process.env.BOUNTY_DIR}`);
+  });
