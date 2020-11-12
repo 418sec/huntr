f64ce97fa996d3e32258b8710cc9ea27268fe329
Adam Nygate <adam@418sec.com>
2020-11-12 09:46:48 +0000

    Added tests to workflow

---
+++ tools/validator/contains-only-new-bounty-files.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import * as core from "@actions/core";
+
+const diff = JSON.parse(process.env.DIFF);
+
+// Check for only new bounties (additions)
+if (diff.filter((item) => item.change === "M" || "D"))
+  core.setFailed("Diff must only contain new bounties.");
---
+++ tools/validator/contains-only-one-bounty.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import {dirname} from "path";
+
+import * as core from "@actions/core";
+
+const diff = JSON.parse(process.env.DIFF);
+
+// Check for many new bounty directories
+const diffDirs = [...new Set(diff.map((item) => dirname(item.path)))];
+if (diffDirs.length > 1)
+  core.setFailed("Diff must only contain one bounty directory.");
