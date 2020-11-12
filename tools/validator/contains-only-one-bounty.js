3512cbae9290fb4c00a1d56e8526529262c9a43f
Adam Nygate <adam@418sec.com>
2020-11-12 11:01:24 +0000

    Implemented vulnerability.json validator

--- tools/validator/contains-only-one-bounty.js
+++ tools/validator/contains-only-one-bounty.js
@@ -8,7 +8,6 @@ const diff = JSON.parse(process.env.DIFF);
 
 // Check for many new bounty directories
 const diffDirs = [...new Set(diff.map((item) => dirname(item.path)))];
-// if (diffDirs.length > 1)
-//   core.setFailed("Diff must only contain one bounty directory.");
-// else
-core.setOutput("bounty_dir", diffDirs[0]);
+if (diffDirs.length > 1)
+  core.setFailed("Diff must only contain one bounty directory.");
+else core.setOutput("bounty_dir", diffDirs[0]);
