a1735c9a38bd469d58327ca46a4a7dc49844cb06
Jamie Slome <jamie@418sec.com>
2020-11-23 14:09:40 +0000

    Epic - Democratization (#1228)

---
+++ tools/process-disclosure/generate-diff.js
@@ -0,0 +1,39 @@
+"use strict";
+import {promisify} from "util";
+import {execFile} from "child_process";
+
+import * as core from "@actions/core";
+
+const promisifiedExecFile = promisify(execFile);
+
+// Get the git diff
+const diffString = await promisifiedExecFile("git", [
+  "diff",
+  "--name-status",
+  process.env.BASE_COMMIT_SHA,
+  process.env.HEAD_COMMIT_SHA,
+  "--",
+]);
+
+// Check for errors
+if (diffString.stderr)
+  core.setFailed(`Error whilst executing git diff: ${diffString.stderr}`);
+
+// Process git diff's stdout
+const diffStringLines = diffString.stdout
+  .split(/[\r\n]+/) // Split by newlines
+  .filter((e) => e); // This removes empty strings
+
+if (diffStringLines.length === 0)
+  core.setFailed(`Diff is empty: ${diffString.stdout}`);
+
+let diff = [];
+diffStringLines.forEach((line) => {
+  const [change, path] = line.split(/\t/);
+  diff.push({
+    change: change,
+    path: path,
+  });
+});
+
+core.setOutput("diff", diff);
