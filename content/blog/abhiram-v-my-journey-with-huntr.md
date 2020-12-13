743211eb9d07f5cdc7a4886c6bf2f9e21d2905ef
Adam Nygate <adam@418sec.com>
2020-12-13 17:58:08 +0000

    feat(blog-list-page): implemented blog list page

--- content/blog/abhiram-v-my-journey-with-huntr.md
+++ content/blog/abhiram-v-my-journey-with-huntr.md
@@ -1,9 +1,16 @@
+---
+title: "Abhiram V: my successful journey with huntr"
+publishedAt: 2020-12-11
+image: https://landen.imgix.net/blog_ihKQMyRvTUWMCgFu/assets/xyITwsXsoXrBeSWj.jpg
+author: Jamie Slome
+---
+
 In this article, we hear from one of our community members, Abhiram V - also referred to as Anon-Artist. He is the Founder of BufferSec and a Cyber Security Researcher at Wattlecorp Cyber Security Labs. Thank you to Abhiram for walking us through his journey on the platform and sharing his experience of collaboration, learning and making a real difference to open-source software.
 
 If you also want to share your experiences on the platform with us, don't hesitate to get in touch. If you have any questions, feel free to contact us.
 
 My fellow huntrs,
-Hello 👋! I'm Abhiram V, a.k.a. Anon-Artist. I am a cyber security enthusiast, an ethical hacker and a part-time bug bounty hunter. I would like to thank the team at huntr for giving me the opportunity to write a blog article on my experiences with huntr. 
+Hello 👋! I'm Abhiram V, a.k.a. Anon-Artist. I am a cyber security enthusiast, an ethical hacker and a part-time bug bounty hunter. I would like to thank the team at huntr for giving me the opportunity to write a blog article on my experiences with huntr.
 Before I jump into my achievements on the platform, I want to start by telling you how this all started. I remember a time when I was fed up with getting many duplicates from major bug bounty platforms and public programs. I was exhausted with the lack of results and decided to take a break from the bug hunting world. Coincidentally, at the exact same time, I received a call from my friend Asjid Kalam who was already a community member on huntr. After spending time catching up, he introduced me to the platform and I felt motivated to get back into bug hunting once again!
 On my first visit to huntr, I was not ready as a lot of the features on the platform utilised GitHub. Another one of my friends, Ajmal Aboobacker (a.k.a B3EF) who is also a bug bounty hunter on the platform, helped me and showed me the ropes when contributing to GitHub and huntr, alike. After this team collaboration, I did in fact see the disclosure explanation video which was surprisingly clear and easy to follow.
 From that day on, I preferred disclosures over fixing as I loved the thrill of finding vulnerabilities in open source packages.
@@ -16,10 +23,11 @@ Disclosure in Cabot
 
 It was a critical bug centered around privilege escalation. After finding this vulnerability, I applied for a CVE and requested that the exploit was added to exploitdb. Within a week, I got the approval! I was so happy to get my first CVE (CVE-2020-7734) and to top it all off, huntr gave me a bounty payment too.
 It was all because of huntr and I always thank them.
-Now, I spend my free time hunting for vulnerabilities in open source with huntr, and have also decided to fix vulnerabilities too. I believe with both of these hand-in-hand, I will improve myself and learning in open source security and bug  hunting.
+Now, I spend my free time hunting for vulnerabilities in open source with huntr, and have also decided to fix vulnerabilities too. I believe with both of these hand-in-hand, I will improve myself and learning in open source security and bug hunting.
+
 ---
 
-Before wrapping up, I would like to mention the community support from huntr. All of the contributors and sheriffs (reviewers) have been awesome and they always support us throughout our journey. 
+Before wrapping up, I would like to mention the community support from huntr. All of the contributors and sheriffs (reviewers) have been awesome and they always support us throughout our journey.
 
 Finally and once again, I want to thank huntr so much for posting my blog article and for their continued support.
 
---
+++ src/pages/blog/index.vue
@@ -0,0 +1,160 @@
+<template>
+  <main class="container mx-auto w-4/5">
+    <section class="py-8 px-4 grid gap-8">
+      <header class="mx-auto md:w-2/3 border-b-4 pb-8 text-center">
+        <h2 class="text-3xl tracking-tight font-extrabold">
+          Welcome to the huntr blog!
+        </h2>
+      </header>
+
+      <article
+        v-for="blog in blogs"
+        :key="blog.slug"
+        class="mx-auto md:w-3/4 border-b pb-8"
+      >
+        <div class="text-xs leading-5 text-gray-500 max-w-prose">
+          <span class="">
+            Published
+            <time :datetime="blog.publishedAtDate">
+              {{ blog.publishedAtFormatted }}
+            </time>
+            by {{ blog.author }}
+          </span>
+          <span class="float-right">{{ blog.readingTimeText }}</span>
+        </div>
+
+        <a href="#" class="mt-2 block">
+          <p class="text-xl font-semibold text-brand-500">
+            {{ blog.title }}
+          </p>
+          <figure v-if="blog.image">
+            <img class="w-full rounded-lg" :src="blog.image" />
+          </figure>
+          <p class="mt-3 text-base text-gray-500">
+            {{ blog.description }}
+          </p>
+        </a>
+        <div class="mt-3">
+          <a
+            href="#"
+            class="text-base font-semibold text-brand-500 hover:text-red-500"
+          >
+            Read full story
+            <svg
+              class="inline w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </a>
+        </div>
+      </article>
+      <footer class="mx-auto w-full md:w-3/4">
+        <nuxt-link
+          v-if="prevPage"
+          :to="{ name: 'blog', query: { page: prevPage } }"
+          class="float-left text-brand-500 hover:text-red-500"
+        >
+          <svg
+            class="inline w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+          Newer posts
+        </nuxt-link>
+
+        <nuxt-link
+          v-if="nextPage"
+          :to="{ name: 'blog', query: { page: nextPage } }"
+          class="float-right text-brand-500 hover:text-red-500"
+        >
+          Older posts
+          <svg
+            class="inline w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            ></path>
+          </svg>
+        </nuxt-link>
+      </footer>
+    </section>
+  </main>
+</template>
+
+<script>
+const { DateTime } = require('luxon')
+const readingTime = require('reading-time')
+const truncate = require('truncate')
+
+export default {
+  watchQuery: ['page'], // Watches the querystring page parameter for updates
+  scrollToTop: true,
+  async asyncData({ $content, query }) {
+    const perPage = 5
+
+    // If no page parameter provided, default to the first page
+    const currentPage =
+      typeof query.page !== 'undefined' ? parseInt(query.page) : 1
+    const totalPages = Math.ceil(
+      (await $content('blog').only([]).fetch()).length / perPage
+    )
+
+    const prevPage = currentPage > 1 ? currentPage - 1 : null
+    const nextPage = currentPage + 1 <= totalPages ? currentPage + 1 : null
+    const skipNumber = (currentPage - 1) * perPage
+
+    // Get this pages' blogs from the content folder
+    const blogs = (
+      await $content('blog', { text: true })
+        .sortBy('createdAt')
+        .limit(perPage)
+        .skip(skipNumber)
+        .fetch()
+    ).map((obj) => ({
+      ...obj,
+      publishedAtDate: obj.publishedAt
+        ? DateTime.fromISO(obj.publishedAt).toISODate()
+        : DateTime.fromISO(obj.createdAt).toISODate(),
+      publishedAtFormatted: obj.publishedAt
+        ? DateTime.fromISO(obj.publishedAt).toLocaleString(DateTime.DATE_FULL)
+        : DateTime.fromISO(obj.createdAt).toLocaleString(DateTime.DATE_FULL),
+      readingTimeText: readingTime(obj.text).text, // Reading time text
+      author: obj.author ? obj.author : 'the huntr team', // Provides a default author
+      description: obj.description // If no description provided, display the truncated body
+        ? obj.description
+        : truncate(obj.text, 300),
+    }))
+
+    return {
+      blogs,
+      prevPage,
+      nextPage,
+    }
+  },
+}
+</script>
