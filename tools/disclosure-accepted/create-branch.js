b637f3389cd9bf6a6d9587e4fa4da7d838460b91
Ben Harvie <benharvie94@gmail.com>
2021-01-13 11:58:05 +0000

    chore(disclosure-accepted): preparing workflow for testing

--- tools/disclosure-accepted/create-branch.js
+++ tools/disclosure-accepted/create-branch.js
@@ -1,7 +1,14 @@
 const branchName = `index-package-username`; // example format
-github.git.createRef({
-  owner: "418sec",
-  repo: process.env.REPO_NAME,
-  ref: `refs/heads/${branchName}`,
-  sha: "", // TODO: Get latest sha from default branch @ 418sec/REPO_NAME
-});
+github.git
+  .createRef({
+    owner: "418sec",
+    repo: process.env.REPO_NAME,
+    ref: `refs/heads/${branchName}`,
+    sha: "", // TODO: Get latest sha from default branch @ 418sec/REPO_NAME
+  })
+  .then((response) => {
+    console.log("Branch created successfully:", branchName);
+  })
+  .catch((error) => {
+    console.log(`Error while trying to created branch '${branchName}':`, error);
+  });
