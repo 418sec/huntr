3fbdda0578b44f813938bc004b7748b6db27eb6b
Ben Harvie <benharvie94@gmail.com>
2021-01-13 15:31:21 +0000

    feat(disclosure-accepted): create-branch basic functionality implemented

--- tools/disclosure-accepted/create-branch.js
+++ tools/disclosure-accepted/create-branch.js
@@ -1,14 +1,34 @@
-const branchName = `index-package-username`; // example format
-github.git
-  .createRef({
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const branchName = `index-package-username`; // TODO: example format
+octokit.repos
+  .get({
     owner: "418sec",
     repo: process.env.REPO_NAME,
-    ref: `refs/heads/${branchName}`,
-    sha: "", // TODO: Get latest sha from default branch @ 418sec/REPO_NAME
   })
-  .then((response) => {
-    console.log("Branch created successfully:", branchName);
+  .then(async (response) => {
+    const defaultBranch = response.data.default_branch;
+    await octokit.git
+      .getRef({
+        owner: "418sec",
+        repo: process.env.REPO_NAME,
+        ref: `heads/${defaultBranch}`,
+      })
+      .then(async (response) => {
+        await github.git.createRef({
+          owner: "418sec",
+          repo: process.env.REPO_NAME,
+          ref: `refs/heads/${branchName}`,
+          sha: response.data.object.sha,
+        });
+      })
+      .then(() => {
+        console.log("Successfully created branch:", branchName);
+      });
   })
   .catch((error) => {
-    console.log(`Error while trying to created branch '${branchName}':`, error);
+    console.log("Error trying to create branch:", error);
   });
