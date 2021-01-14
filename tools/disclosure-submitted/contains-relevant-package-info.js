3553227a14856cd7a8d82779609beeeb3dd44e9b
JamieSlome <jamie@418sec.com>
2021-01-14 19:39:52 +0000

    chore(clean-up): negated order of relevant package info

--- tools/disclosure-submitted/contains-relevant-package-info.js
+++ tools/disclosure-submitted/contains-relevant-package-info.js
@@ -12,7 +12,7 @@ const graphQLClient = new GraphQLClient(process.env.GRAPHQL_API_URL, {
 
 const diff = JSON.parse(process.env.DIFF);
 
-const jsonPath = "../../" + diff.filter(file => 
+const jsonPath = "../../" + diff.filter(file =>
     file.path.includes("vulnerability.json")
 )[0].path;
 
@@ -25,36 +25,36 @@ const jsonContents = await fs
 
 const packageRegistry = jsonContents.Package.Registry === "pip" ? "pypi" : jsonContents.Package.Registry;
 
-if(packageRegistry === "other") {
-    return;
-}
+if (!packageRegistry === "other") {
 
-const packageName = jsonContents.Package.Name;
-const packageUrl = jsonContents.Package.URL;
+    const packageName = jsonContents.Package.Name;
+    const packageUrl = jsonContents.Package.URL;
 
-const repositoryOwner = jsonContents.Repository.Owner;
-const repositoryName = jsonContents.Repository.Name;
+    const repositoryOwner = jsonContents.Repository.Owner;
+    const repositoryName = jsonContents.Repository.Name;
 
-const query = gql`
+    const query = gql`
             getPackageDetails(PackageName:"${packageName}", PackageManager:"${packageRegistry}")
         `
 
-const packageDetails = await graphQLClient.request(query)
-    .then(JSON.parse)
-    .catch(() => {
-        core.setFailed(
-            `The package name (${packageName}) and/or package registry (${packageRegistry}) could not be found...`
-        )
-    });
+    const packageDetails = await graphQLClient.request(query)
+        .then(JSON.parse)
+        .catch(() => {
+            core.setFailed(
+                `The package name (${packageName}) and/or package registry (${packageRegistry}) could not be found...`
+            )
+        });
 
-if(packageDetails.package.url !== packageUrl) {
-    core.warning(`Suggestion - Should your Package.URL be: ${packageDetails.package.url}?`)
-    core.setFailed(`The PR does not contain a valid registry URL...`)
+    if (packageDetails.package.url !== packageUrl) {
+        core.warning(`Suggestion - should your Package.URL be: ${packageDetails.package.url}?`)
+        core.setFailed(`The PR does not contain a valid registry URL...`)
+    }
+
+    if (packageDetails.repository.owner !== repositoryOwner && packageDetails.repository.name !== repositoryName) {
+        core.warning(`Suggestion - should your Repository.Owner and Repository.Name be: ${packageDetails.repository.owner}, ${packageDetails.repository.name}?`)
+        core.setFailed(`The PR does not contain a valid registry URL...`)
+    }
+
+} else {
+    core.warning(`Relevant package info. validation skipped...`)
 }
-
-if(packageDetails.repository.owner !== repositoryOwner && packageDetails.repository.name !== repositoryName) {
-    core.warning(`Suggestion - Should your Repository.Owner and Repository.Name be: ${packageDetails.repository.owner}, ${packageDetails.repository.name}?`)
-    core.setFailed(`The PR does not contain a valid registry URL...`)
-}
-
-
