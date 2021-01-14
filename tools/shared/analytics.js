1baa4cbfa2472ec760a1c64ba77a7f4fd9d602d7
JamieSlome <jamie@418sec.com>
2021-01-14 17:33:41 +0000

    fix(analytics): negated logic to check if user is not staff

--- tools/shared/analytics.js
+++ tools/shared/analytics.js
@@ -4,13 +4,12 @@ const heap = require("heap-api")(`${process.env.HEAP_ID}`);
 
 const staffIds = ["55323451", "4394852", "17127410", "34353676", "28839565", "63112452", "61279246"]
 
-if(staffIds.includes(process.env.USER_ID)) {
-    core.warning(`This PR was submitted by a staff member...`)
-    core.setFailed(0);
+if(!staffIds.includes(process.env.USER_ID)) {
+    heap.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
+        name: process.env.PACKAGE_NAME
+    }).then(() => {
+        console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
+    })
+} else {
+    core.warning(`Analytics skipped...`)
 }
-
-heap.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
-    name: process.env.PACKAGE_NAME
-}).then(() => {
-    console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
-})
