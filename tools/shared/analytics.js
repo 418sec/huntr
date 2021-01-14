2845e1cdaf46e318aaee092f6e490cda4650eab8
Jamie Slome <jamie@418sec.com>
2021-01-14 17:04:07 +0000

    chore(fail-action): fail action programmatically to-do

--- tools/shared/analytics.js
+++ tools/shared/analytics.js
@@ -1,10 +1,12 @@
+import * as core from "@actions/core";
+
 const heap = require("heap-api")(`${process.env.HEAP_ID}`);
 
 const staffIds = ["55323451", "4394852", "17127410", "34353676", "28839565", "63112452", "61279246"]
 
 if(staffIds.includes(process.env.USER_ID)) {
     core.warning(`This PR was submitted by a staff member...`)
-    return;
+    core.setFailed(0);
 }
 
 heap.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
