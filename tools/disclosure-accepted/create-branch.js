db9f13c3ebf15b09b158c901b9555c68495deba5
Ben Harvie <benharvie94@gmail.com>
2021-01-14 12:28:54 +0000

    fix(disclosure-accepted): refactored get-repository-info > get-vulnerability-json

--- tools/disclosure-accepted/create-branch.js
+++ tools/disclosure-accepted/create-branch.js
@@ -1,5 +1,6 @@
 "use strict";
 import { Octokit } from "@octokit/rest";
+import { Cipher } from "crypto";
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
@@ -8,14 +9,17 @@ const vulnerabilityJson = JSON.parse(process.env.VULNERABILITY_JSON);
 const repoName = vulnerabilityJson.Repository.Name;
 const branchName = `index-package-username`; // TODO: example format, represents unique branch for each submission
 
+console.log("Fetching the default branch for:", `418sec/${repoName}`);
 // Get the repo's default branch
 octokit.repos
   .get({
     owner: "418sec",
-    repo: process.env.REPO_NAME,
+    repo: repoName,
   })
   .then(async (response) => {
     const defaultBranch = response.data.default_branch;
+    console.log("Default branch found:", defaultBranch);
+
     // Get the latest sha commit
     await octokit.git
       .getRef({
@@ -24,13 +28,15 @@ octokit.repos
         ref: `heads/${defaultBranch}`,
       })
       .then(async (response) => {
+        const latestSha = response.data.object.sha;
+        console.log("Latest commit SHA fetched:", sha);
         // Create the new branch
         await github.git
           .createRef({
             owner: "418sec",
             repo: repoName,
             ref: `refs/heads/${branchName}`,
-            sha: response.data.object.sha,
+            sha: latestSha,
           })
           .then(() => {
             console.log("Successfully created branch:", branchName);
