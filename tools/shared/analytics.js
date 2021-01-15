d4b2735f4010a7d98316cda7a4773d21c98c44c6
Jamie Slome <jamie@418sec.com>
2021-01-15 12:12:38 +0000

    fix(import): importing heap as opposed to require

--- tools/shared/analytics.js
+++ tools/shared/analytics.js
@@ -1,11 +1,13 @@
 import * as core from "@actions/core";
 
-const heap = require("heap-api")(`${process.env.HEAP_ID}`);
+import * as heap from "heap-api";
+
+const analytics = heap(`${process.env.HEAP_ID}`);
 
 const staffIds = ["55323451", "4394852", "17127410", "34353676", "28839565", "63112452", "61279246"]
 
 if(!staffIds.includes(process.env.USER_ID)) {
-    heap.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
+    analytics.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
         name: process.env.PACKAGE_NAME
     }).then(() => {
         console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
