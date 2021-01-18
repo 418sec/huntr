9ce6c78adb97e1f13bf271b73e7a9fe87623f57f
Ben Harvie <benharvie94@gmail.com>
2021-01-18 15:29:33 +0000

    fix(disclosure-accepted): removed unnecessary async()

--- tools/disclosure-accepted/create-branch.js
+++ tools/disclosure-accepted/create-branch.js
@@ -14,52 +14,48 @@ const packageRegistry = vulnerabilityJson.Package.Registry;
 const branchName = `${bountyIndex}-${packageRegistry}-${packageName}`;
 console.log("Creating branch:", branchName);
 
-async () => {
-  // Get the repo's default branch
-  console.log("Fetching the default branch for:", `418sec/${repoName}`);
-  const {
-    data: { default_branch },
-  } = await octokit.repos
-    .get({
-      owner: "418sec",
-      repo: repoName,
-    })
-    .catch((error) => {
-      core.setFailed(`Error attempting to find the default branch: ${error}`);
-    });
+// Get the repo's default branch
+console.log("Fetching the default branch for:", `418sec/${repoName}`);
+const {
+  data: { default_branch },
+} = await octokit.repos
+  .get({
+    owner: "418sec",
+    repo: repoName,
+  })
+  .catch((error) => {
+    core.setFailed(`Error attempting to find the default branch: ${error}`);
+  });
 
-  // Get the latest commit SHA
-  const {
-    data: {
-      object: { sha },
-    },
-  } = await octokit.git
-    .getRef({
-      owner: "418sec",
-      repo: repoName,
-      ref: `heads/${default_branch}`,
-    })
-    .catch((error) => {
-      core.setFailed(
-        `Error attempting to fetch the latest commit SHA: ${error}`
-      );
-    });
+// Get the latest commit SHA
+const {
+  data: {
+    object: { sha },
+  },
+} = await octokit.git
+  .getRef({
+    owner: "418sec",
+    repo: repoName,
+    ref: `heads/${default_branch}`,
+  })
+  .catch((error) => {
+    core.setFailed(`Error attempting to fetch the latest commit SHA: ${error}`);
+  });
 
-  // Create the new branch
-  await octokit.git
-    .createRef({
-      owner: "418sec",
-      repo: repoName,
-      ref: `refs/heads/${branchName}`,
-      sha: sha,
-    })
-    .then((response) => {
-      console.log(
-        `Successfully created branch '${branchName}' on 418sec/${defaultBranch}:`,
-        response
-      );
-    })
-    .catch((error) => {
-      core.setFailed(`Error attempting to create a new branch: ${error}`);
-    });
-};
+// Create the new branch
+await octokit.git
+  .createRef({
+    owner: "418sec",
+    repo: repoName,
+    ref: `refs/heads/${branchName}`,
+    sha: sha,
+  })
+  .then((response) => {
+    console.log(
+      `Successfully created branch '${branchName}' on 418sec/${defaultBranch}:`,
+      response
+    );
+  })
+  .catch((error) => {
+    core.setFailed(`Error attempting to create a new branch: ${error}`);
+  });
