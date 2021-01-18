7b814181eac733b5a6914522de3e73d0af746390
Jamie Slome <jamie@418sec.com>
2021-01-18 17:27:02 +0000

    chore(fix): corrected string comparison on analytics.js

--- tools/shared/analytics.js
+++ tools/shared/analytics.js
@@ -3,8 +3,9 @@ import heap from "heap-api";
 
 const analytics = heap(`${process.env.HEAP_ID}`);
 
-if(process.env.USER_ID !== "63112452" || process.env.USER_ID !== "61279246") {
-
+if (`${process.env.USER_ID}` == "63112452" || `${process.env.USER_ID}` == "61279246") {
+    core.warning("Analytics skipped...");
+} else {
     await analytics.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
         name: process.env.PACKAGE_NAME
     }).then(() => {
@@ -12,6 +13,4 @@ if(process.env.USER_ID !== "63112452" || process.env.USER_ID !== "61279246") {
     }).catch(() => {
         core.warning("Analytics call failed...");
     })
-} else {
-    core.warning("Analytics skipped...");
 }
