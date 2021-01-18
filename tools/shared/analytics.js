37d324097635c13e9c0ffc523458025259309aa9
Jamie Slome <jamie@418sec.com>
2021-01-18 09:42:39 +0000

    chore(staff): remove staff from disclosure submitted analytics

--- tools/shared/analytics.js
+++ tools/shared/analytics.js
@@ -4,14 +4,8 @@ import heap from "heap-api";
 
 const analytics = heap(`${process.env.HEAP_ID}`);
 
-const staffIds = ["55323451", "4394852", "17127410", "34353676", "28839565", "63112452", "61279246"]
-
-if(!staffIds.includes(process.env.USER_ID)) {
-    await analytics.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
-        name: process.env.PACKAGE_NAME
-    }).then(() => {
-        console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
-    })
-} else {
-    core.warning(`Analytics skipped...`)
-}
+await analytics.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
+    name: process.env.PACKAGE_NAME
+}).then(() => {
+    console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
+})
