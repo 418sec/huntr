5b003ff455b7e6e77b72a3ccff446637be486cf1
Jamie Slome <jamie@418sec.com>
2021-01-18 17:11:24 +0000

    chore(analytics): no longer tracking huntr-helper or tester

--- tools/shared/analytics.js
+++ tools/shared/analytics.js
@@ -3,10 +3,15 @@ import heap from "heap-api";
 
 const analytics = heap(`${process.env.HEAP_ID}`);
 
-await analytics.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
-    name: process.env.PACKAGE_NAME
-}).then(() => {
-    console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
-}).catch(() => {
-    core.warning("Analytics call failed...");
-})
+if(process.env.USER_ID !== "63112452" || process.env.USER_ID !== "61279246") {
+
+    await analytics.track(process.env.EVENT_NAME, `github_${process.env.USER_ID}`, {
+        name: process.env.PACKAGE_NAME
+    }).then(() => {
+        console.log(`Track ${process.env.EVENT_NAME} for user (${process.env.USER_ID}) on ${process.env.PACKAGE_NAME}`)
+    }).catch(() => {
+        core.warning("Analytics call failed...");
+    })
+} else {
+    core.warning("Analytics skipped...");
+}
